a3ed8b4c7d4fd3177c0fb8b9ab3560375e56ee60
QUEILA SOUTO <73967682+Queila-Souto@users.noreply.github.com>
2025-05-01 17:12:33 -0300

    Configurando botões

--- app/src/main/java/devandroid/queila/applistaalunos/view/MainActivity.java
+++ app/src/main/java/devandroid/queila/applistaalunos/view/MainActivity.java
@@ -3,7 +3,10 @@ package devandroid.queila.applistaalunos.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import devandroid.queila.applistaalunos.R;
 import devandroid.queila.applistaalunos.model.Pessoa;
@@ -15,6 +18,9 @@ EditText edittxtnome;
 EditText edittxtsobrenome;
 EditText edittxtcurso;
 EditText edittxttelefone;
+Button btnlimpar;
+Button btnsalvar;
+Button btnfinalizar;
 
 
     @Override
@@ -31,11 +37,45 @@ EditText edittxttelefone;
         edittxtsobrenome = findViewById(R.id.editTextText3);
         edittxtcurso = findViewById(R.id.editTextText4);
         edittxttelefone = findViewById(R.id.editTextText5);
+        btnlimpar = findViewById(R.id.buttonLimpar);
+        btnsalvar = findViewById(R.id.buttonSalvar);
+        btnfinalizar = findViewById(R.id.buttonFinalizar);
 
         edittxtnome.setText(pessoa.getPrimeiroNome());
         edittxtsobrenome.setText(pessoa.getSobrenome());
         edittxtcurso.setText(pessoa.getCurso());
         edittxttelefone.setText(pessoa.getTelefone());
+        btnlimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittxtnome.setText("");
+                edittxtsobrenome.setText("");
+                edittxtcurso.setText("");
+                edittxttelefone.setText("");
+            }
+        });
+        btnfinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Até logo!",Toast.LENGTH_LONG).show();
+            finish();
+            }
+        });
+        btnsalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pessoa.setPrimeiroNome(edittxtnome.getText().toString());
+                pessoa.setSobrenome(edittxtsobrenome.getText().toString());
+                pessoa.setTelefone(edittxttelefone.getText().toString());
+                pessoa.setCurso(edittxtcurso.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Salvo"+pessoa.toString(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+
 
     }
 }
