c144cf0bb25d6dfbe992a5e857b71e8842f66dd1
QUEILA SOUTO <73967682+Queila-Souto@users.noreply.github.com>
2025-06-07 17:38:03 -0300

    Configuração objetos tela CadastroUsuario.java

--- app/src/main/java/devandroid/queila/applistaalunos/view/CadastroUsuario.java
+++ app/src/main/java/devandroid/queila/applistaalunos/view/CadastroUsuario.java
@@ -1,6 +1,9 @@
 package devandroid.queila.applistaalunos.view;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +14,35 @@ import androidx.core.view.WindowInsetsCompat;
 import devandroid.queila.applistaalunos.R;
 
 public class CadastroUsuario extends AppCompatActivity {
+    Button buttonVoltar;
+    ImageView buttonCadastrar;
+    EditText editTextName;
+    EditText editTextLogin;
+    EditText editTextPassword;
+    EditText editTextConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_usuario);
+        incializarObjetos();
+        configurarBotoes();
 
         }
+
+    private void configurarBotoes() {
+        buttonVoltar.setOnClickListener(v->{
+            finish();
+        });
     }
+
+    private void incializarObjetos() {
+        buttonVoltar = findViewById(R.id.buttonVoltar);
+        buttonCadastrar = findViewById(R.id.imageView3);
+        editTextLogin = findViewById(R.id.editTextLogin);
+        editTextName = findViewById(R.id.editTextName);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+
+    }
+}
