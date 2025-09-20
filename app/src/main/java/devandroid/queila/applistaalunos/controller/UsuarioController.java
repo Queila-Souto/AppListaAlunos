3675bae20c1f4347e862bd999751c4335ba94c01
QUEILA SOUTO <73967682+Queila-Souto@users.noreply.github.com>
2025-09-20 17:48:49 -0300

    Adiciona funcionalidade de login e autenticação
    Este commit introduz as seguintes mudanças:
    
    - **API de Autenticação:**
        - Criada a interface `Auth.java` com o endpoint `/usuario/login` para autenticação de usuários.
    - **Modelo de Resposta do Login:**
        - Adicionado o modelo `LoginResponse.java` para representar a resposta da API de login, contendo o token de autenticação.
    - **Lógica de Login na View:**
        - Em `Login.java`, foi implementada a lógica para capturar email e senha e chamar o método `login` do `UsuarioController`.
    - **Controller de Usuário:**
        - Em `UsuarioController.java`, foi adicionado o método `login` que realiza a chamada à API de autenticação.
        - O token recebido é armazenado em `SharedPreferences` para uso posterior.
        - Foram adicionados logs para depuração do processo de autenticação.
    - **Manifest:**
        - A `Activity` inicial foi alterada de `MainActivity` para `Login` no `AndroidManifest.xml`.
        - A `MainActivity` foi registrada como uma `Activity` exportada como `false`.

--- app/src/main/java/devandroid/queila/applistaalunos/controller/UsuarioController.java
+++ app/src/main/java/devandroid/queila/applistaalunos/controller/UsuarioController.java
@@ -1,6 +1,9 @@
 package devandroid.queila.applistaalunos.controller;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -9,11 +12,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import devandroid.queila.applistaalunos.api.AlunoApi;
+import devandroid.queila.applistaalunos.api.Auth;
 import devandroid.queila.applistaalunos.api.PessoaCallBack;
 import devandroid.queila.applistaalunos.api.RetrofitClient;
 import devandroid.queila.applistaalunos.api.UsuarioApi;
 import devandroid.queila.applistaalunos.api.UsuarioCallBack;
 import devandroid.queila.applistaalunos.model.Aluno;
+import devandroid.queila.applistaalunos.model.LoginRequest;
+import devandroid.queila.applistaalunos.model.LoginResponse;
 import devandroid.queila.applistaalunos.model.Usuario;
 import devandroid.queila.applistaalunos.view.PessoaAdapter;
 import retrofit2.Call;
@@ -22,8 +28,47 @@ import retrofit2.Response;
 
 public class UsuarioController {
     private UsuarioApi usuarioApi = RetrofitClient.getRetrofitInstance().create(UsuarioApi.class);
+    private String email;
+    private String password;
 
+    public void login(String email, String password, Context context) {
+        Log.d("AUTH", "ACESSEI METODO LOGIN");
+
+        LoginRequest loginRequest = new LoginRequest(email, password);
+        Auth authApi = RetrofitClient.getRetrofitInstance().create(Auth.class);
+        SharedPreferences preferences = context.getSharedPreferences("APP_PREFS", MODE_PRIVATE);
+
+        authApi.login(loginRequest).enqueue(
+
+        new Callback<LoginResponse>(){
+                @Override
+                public void onResponse(@NonNull Call<LoginResponse> call, @NonNull Response<LoginResponse> response) {
+                    Log.d("AUTH", "ACESSEI CALLBACK");
+
+                    if (response.isSuccessful() && response.body() != null) {
+                        Log.d("AUTH", "RESPOSTA"+response);
+                        String token = response.body().getToken();
+                        Log.d("AUTH", "Token: " + token);
+                        preferences.edit()
+                                .putString("AUTH_TOKEN", token)
+                                .apply();
+
+                    }
+                    else {
+                        Log.d("AUTH", "EStou no else da resposta  -- "+response);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<LoginResponse> call, Throwable t) {
+                    Log.e("AUTH", "Não foi possível recuperar o token" );
+          }
+
+    }
+    );
+    }
     public void salvarBD(Usuario usuario, UsuarioCallBack usuarioCallBack) {
+
         Call<Void> call = usuarioApi.cadastrar(usuario);
         call.enqueue(new Callback<Void>() {
 
--- app/src/main/java/devandroid/queila/applistaalunos/view/Login.java
+++ app/src/main/java/devandroid/queila/applistaalunos/view/Login.java
@@ -1,17 +1,21 @@
 package devandroid.queila.applistaalunos.view;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import devandroid.queila.applistaalunos.R;
+import devandroid.queila.applistaalunos.controller.UsuarioController;
+import devandroid.queila.applistaalunos.model.LoginRequest;
 
 public class Login extends AppCompatActivity {
     EditText editTextEmail;
     EditText editTextSenha;
     ImageView buttonLogar;
     TextView buttonNovoUsuario;
+
 
 
     @Override
@@ -26,6 +30,11 @@ public class Login extends AppCompatActivity {
         buttonNovoUsuario.setOnClickListener(v -> {
         });
         buttonLogar.setOnClickListener(v -> {
+           Log.e("AUTH","APERTEI O BOTAO");
+            String email = editTextEmail.getText().toString();
+            String senha = editTextSenha.getText().toString();
+            UsuarioController usuarioController = new UsuarioController();
+            usuarioController.login(email,senha,this);
         });
     }
 
